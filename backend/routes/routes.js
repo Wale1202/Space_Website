d8fec48ed632e06c518d64ff4bca2c1bfaf0a87f
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2025-06-23 21:18:36 +0100

    fixes to the backend link

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const { getAPOD} = require('../controllers/controller');
 
 // API
+// to check if api is working
+router.get('/', (req, res) => {
+  res.json({ 
+    status: 'API is working',
+    message: 'Welcome to NASA API Explorer',
+    endpoints: {
+      apod: '/api/apod',
+      test: '/api/test'
+    }
+  });
+});
 router.get('/apod',getAPOD);
 
 router.get('/test', (req, res) => res.json({ message: "Router is working"}));
