8fd9765e8b435bbe3974f22b8a3bfc869a9a85df
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2025-06-24 22:25:51 +0100

    updates on availabe api like apod

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,22 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { getAPOD} = require('../controllers/controller');
+const { 
+  getAPOD, 
+  getMarsPhotos, 
+  getEPIC, 
+  getNeoWs, 
+  searchNasaLibrary 
+} = require('../controllers/controller');
 
 // API
 // to check if api is working
-router.get('/', (req, res) => {
-  res.json({ 
-    status: 'API is working',
-    message: 'Welcome to NASA API Explorer',
-    // endpoints: {
-    //   apod: '/',
-    //   test: '/'
-    // }
-  });
-});
-router.get('/apod',getAPOD);
-
-router.get('/test', (req, res) => res.json({ message: "Router is working"}));
+// NASA API endpoints
+router.get('/apod', getAPOD);
+// router.get('/mars-photos', getMarsPhotos);
+// router.get('/epic', getEPIC);
+// router.get('/neows', getNeoWs);
+// router.get('/search', searchNasaLibrary);
 
 
 module.exports = router;
