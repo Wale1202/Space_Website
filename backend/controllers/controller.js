2babcd0e92ddf7d0d7b2ced6414c92ecbcb3ac3d
Wale1202 <83563997+Wale1202@users.noreply.github.com>
2025-06-26 22:00:41 +0100

    addition of mars rover api

--- backend/controllers/controller.js
+++ backend/controllers/controller.js
@@ -18,3 +18,87 @@ exports.getAPOD = async (req, res, next) => {
     next(error);
   }
 };
+/**
+ * Get Mars Rover Photos
+ */
+// This Gets Mars Rover Photos
+exports.getMarsPhotos = async (req, res, next) => {
+  try { 
+    const { rover = 'curiosity', ...params } = req.query;
+    const cacheKey = `mars-${rover}-${JSON.stringify(params)}`;
+    
+    const data = await getCachedData(cacheKey, async () => {
+      return fetchNasaApi(`/mars-photos/api/v1/rovers/${rover}/photos`, params);
+    });
+    
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+// This Gets PhotosEarth Polychromatic Imaging Camera
+exports.getEPIC = async (req, res, next) => {
+  try {
+    const { collection = 'natural', date, ...params } = req.query;
+    let endpoint = `/EPIC/api/${collection}`;
+    
+    if (date) {
+      endpoint += `/date/${date}`;
+    }
+    
+    const cacheKey = `epic-${collection}-${date || 'latest'}-${JSON.stringify(params)}`;
+    
+    const data = await getCachedData(cacheKey, async () => {
+      return fetchNasaApi(endpoint, params);
+    });
+    
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+// This Gets Near Earth Object Web Service
+exports.getNeoWs = async (req, res, next) => {
+  try {
+    const { start_date, end_date, ...params } = req.query;
+    
+    // Use the feed endpoint if both dates are provided
+    let endpoint = '/neo/rest/v1/feed';
+    let queryParams = { ...params };
+    
+    if (start_date && end_date) {
+      queryParams = { ...queryParams, start_date, end_date };
+    } else {
+      // Uses today endpoint
+      endpoint = '/neo/rest/v1/feed/today';
+    }
+    
+    const cacheKey = `neows-${JSON.stringify(queryParams)}`;
+    
+    const data = await getCachedData(cacheKey, async () => {
+      return fetchNasaApi(endpoint, queryParams);
+    });
+    
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+};
+// This Gets the Search NASA Image and Video Library
+exports.searchNasaLibrary = async (req, res, next) => {
+  try {
+    const cacheKey = `library-search-${JSON.stringify(req.query)}`;
+    
+    const data = await getCachedData(cacheKey, async () => {
+      // NASA Image and Video Library uses a different base URL
+      const axios = require('axios');
+      const url = `https://images-api.nasa.gov/search?${new URLSearchParams(req.query).toString()}`;
+      const response = await axios.get(url);
+      return response.data;
+    });
+    
+    res.json(data);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,20 +2,20 @@ const express = require('express');
 const router = express.Router();
 const { 
   getAPOD, 
-  //getMarsPhotos, 
-  //getEPIC, 
-  //getNeoWs, 
-  //searchNasaLibrary 
+  getMarsPhotos, 
+  getEPIC, 
+  getNeoWs, 
+  searchNasaLibrary 
 } = require('../controllers/controller');
 
 // API
 // to check if api is working
 // NASA API endpoints
 router.get('/apod', getAPOD);
-// router.get('/mars-photos', getMarsPhotos);
-// router.get('/epic', getEPIC);
-// router.get('/neows', getNeoWs);
-// router.get('/search', searchNasaLibrary);
+router.get('/mars-photos', getMarsPhotos);
+router.get('/epic', getEPIC);
+router.get('/neows', getNeoWs);
+router.get('/search', searchNasaLibrary);
 console.log('ROUTES:', router.stack.map(r => r.route?.path));
 
 
